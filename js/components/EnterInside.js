e729a15ef6d25a3952907d8b5195324135ee4c23
Miszczoo <mcglowack@gmail.com>
2021-02-14 17:57:33 +0100

    refactor EnterInside class, add YouTube player API in html and configure it, add loading screen

--- js/components/EnterInside.js
+++ js/components/EnterInside.js
@@ -1,28 +1,36 @@
+const welcomeButton = document.querySelector('.welcome-page__button');
+const header = document.querySelector('header');
+const homePage = document.querySelector('.main-wrapper__home');
+const welcomePage = document.querySelector('.welcome-page');
+const mainWrapper = document.querySelector('.main-wrapper');
+const footer = document.querySelector('footer');
+const loadingPage = document.querySelector('.loading-page');
+const square = document.querySelector('.loading-page__loading-square')
+
 export class EnterInside{
   constructor(){
-    this.addClass();
+    this.loadingPage();
   }
+ 
+  loadingPage(){
+    welcomeButton.addEventListener('click', () => {
+      loadingPage.classList.add('active');
+      square.classList.add('active');
+      welcomePage.style.display = 'none';
+      setTimeout(addClass, 3000);
+    });
 
-  addClass(){
-    const header = document.querySelector('header');
-    const homePage = document.querySelector('.main-wrapper__home');
-    const welcomePage = document.querySelector('.welcome-page');
-    const welcomeButton = document.querySelector('.welcome-page__button');
-    const mainWrapper = document.querySelector('.main-wrapper');
-    const footer = document.querySelector('footer');
-
-    // console.log(welcomeButton);
-    welcomeButton.addEventListener('click', ()=> {
-        header.classList.add('active');
-        homePage.classList.add('active');
-        mainWrapper.classList.add('active');
-        footer.classList.add('active');
-        welcomePage.style.display = 'none';
-        // console.log('Greeetings sire!');
-      })
-
-    // console.log("im alive!");
+    const addClass = () => {  
+      console.log("pting");
+      header.classList.add('active');
+      homePage.classList.add('active');
+      mainWrapper.classList.add('active');
+      footer.classList.add('active');
+      loadingPage.classList.remove('active');
+      square.classList.remove('active')
+    }
   }
+  
 }
 
 export const enterInsidePage = new EnterInside();
