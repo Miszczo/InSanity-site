3b3b5ccba8a94229d060d6ab0eddec47b4f26510
Miszczoo <mcglowack@gmail.com>
2021-03-13 17:42:29 +0100

    Add hamburger onclick functions - hide menu when click on section

--- js/components/HamburgerSwitch.js
+++ js/components/HamburgerSwitch.js
@@ -1,20 +1,34 @@
+const hamburgerSelector = document.querySelector('.hamburger-button-selector');
+const hiddenMenuSelector = document.querySelector('.hidden-menu');
+const socialMediaHEaderIcons = document.querySelector('.social-media__header-mobile');
+const headerButtonSelector = document.querySelectorAll('.header-button-selector');
+
 class HamburgerSwitch {
   constructor() {
     this.hamburgerToggle();
+    this.hideMenuAfterSectionChoose();
   }
+
   hamburgerToggle() {
-    const hamburgerSelector = document.querySelector('.hamburger-button-selector');
-    const hiddenMenuSelector = document.querySelector('.hidden-menu');
-    const socialMediaHEaderIcons = document.querySelector('.social-media__header-mobile');
-    
+ 
     console.log(hamburgerSelector);
     console.log(hiddenMenuSelector);
-
+    console.log(headerButtonSelector);
     hamburgerSelector.addEventListener('click', () => {
       hiddenMenuSelector.classList.toggle('post-active');
       socialMediaHEaderIcons.classList.toggle('post');
       console.log('click');
     });
+  }
+
+  hideMenuAfterSectionChoose(){
+    for(let button of headerButtonSelector){
+      button.addEventListener('click', ()=> {
+        hiddenMenuSelector.classList.toggle('post-active');
+        socialMediaHEaderIcons.classList.toggle('post');
+        console.log('click');
+      });
+    }
   }
 }
 
